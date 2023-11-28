8eadf9311dbf6890aba191e1f99e0dcbf1f57412
Maxim Golovin <boss@primestr.ru>
2023-11-28 16:56:34 +0500

    Add users app. Add JWT auth. Add views and serializers for it.

---
+++ backend/api/v1/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView,
+                                            TokenVerifyView)
+
+from api.v1.views import AuthViewSet
+from users.views import CustomUserViewSet
+
+router = DefaultRouter()
+
+router.register(r'users', CustomUserViewSet)
+router.register(r'auth', AuthViewSet, basename='auth')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/login/', TokenObtainPairView.as_view(),
+         name='login'),
+    path('auth/logout/', AuthViewSet, name='logout'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
