44278eef3f6ec1ccee0bb6f7d75c6cdf1ad5cb36
Maxim Golovin <boss@primestr.ru>
2023-12-02 01:25:54 +0500

    Rebuild celery. Edit MatchSerializer. Edit Analysis ViewSet and urls.

---
+++ backend/api/v1/tasks.py
@@ -0,0 +1,61 @@
+import asyncio
+import logging
+
+from telegram import Bot
+
+from backend.celery import app
+from core.environment import BOT_TOKEN
+from products.models import MatchingPredictions
+
+logger = logging.getLogger(__name__)
+
+
+@app.task
+async def send_telegram_message(chat_id, message):
+    bot = Bot(token=BOT_TOKEN)
+    if chat_id is not None:
+        try:
+            await bot.send_message(chat_id=chat_id, text=message)
+            logger.info('Сообщение в Telegram успешно отправлено!')
+        except Exception as err:
+            logger.error(f'Сообщение не отправлено! Текст ошибки: {err}')
+
+    # TODO: Если у пользователя не задан TELEGRAM_ID - отправить письмо
+    #  на почту.
+    logger.warning(f'Сообщение не отправлено! '
+                   f'Не задан Telegram ID пользователя! '
+                   f'Отправляем письмо на почту...')
+
+    try:
+        # TODO: Отправить письмо
+        pass
+    except Exception as error:
+        pass
+        # TODO: Дать в логи ошибку если не отправилось.
+
+
+@app.task
+def make_predictions(dealer_data, chat_id=None):
+    try:
+        # TODO: Сюда ставим вызов ML модели, передаём в неё массив для анализа.
+
+        # ml_results = start_ml(dealer_data,
+        #                       prosept_products,
+        #                       matches)
+
+        # Обработка результатов ML и создание записей в БД
+        # for dealer_product_id, prosept_product_ids in ml_results.items():
+        #     for prosept_product_id in prosept_product_ids:
+        #         MatchingPredictions.objects.create(
+        #             dealer_product_id=dealer_product_id,
+        #             prosept_product_id=prosept_product_id
+        #         )
+
+        message = 'Расчёт соответствий завершён успешно.'
+        logger.info(message)
+
+    except Exception as error:
+        message = f'Расчёт завершён с ошибкой: {error}'
+        logger.error(message)
+
+    asyncio.run(send_telegram_message(chat_id, message))
--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,69 +1,11 @@
-import asyncio
-import logging
+import os
 
 from celery import Celery
-from telegram import Bot
 
-from core.environment import BOT_TOKEN
-# from products.models import MatchingPredictions
-
-logger = logging.getLogger(__name__)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('celery', broker='redis://redis:6379/0')
 
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-@app.task
-async def send_telegram_message(chat_id, message):
-    bot = Bot(token=BOT_TOKEN)
-    if chat_id is not None:
-        try:
-            await bot.send_message(chat_id=chat_id, text=message)
-            logger.info('Сообщение в Telegram успешно отправлено!')
-        except Exception as err:
-            logger.error(f'Сообщение не отправлено! Текст ошибки: {err}')
-
-    # TODO: Если у пользователя не задан TELEGRAM_ID - отправить письмо
-    #  на почту.
-    logger.warning(f'Сообщение не отправлено! '
-                   f'Не задан Telegram ID пользователя! '
-                   f'Отправляем письмо на почту...')
-
-    try:
-        # TODO: Отправить письмо
-        pass
-    except Exception as error:
-        pass
-        # TODO: Дать в логи ошибку если не отправилось.
-
-
-@app.task
-def make_predictions(dealer_data,
-                     prosept_products,
-                     dealers,
-                     matches,
-                     chat_id=None):
-    try:
-        # TODO: Сюда ставим вызов ML модели, передаём в неё массив для анализа.
-
-        # ml_results = start_ml(dealer_data,
-        #                       prosept_products,
-        #                       dealers,
-        #                       matches)
-
-
-        # Обработка результатов ML и создание записей в БД
-        # for dealer_product_id, prosept_product_ids in ml_results.items():
-        #     for prosept_product_id in prosept_product_ids:
-        #         MatchingPredictions.objects.create(
-        #             dealer_product_id=dealer_product_id,
-        #             prosept_product_id=prosept_product_id
-        #         )
-
-        message = 'Расчёт совпадений завершён успешно.'
-        logger.info(message)
-
-    except Exception as error:
-        message = f'Расчёт завершён с ошибкой: {error}'
-        logger.error(message)
-
-    asyncio.run(send_telegram_message(chat_id, message))
+app.autodiscover_tasks()
