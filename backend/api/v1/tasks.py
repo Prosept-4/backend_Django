074ec0fe306b45d56f8bf7d42949e93eab76b813
Maxim Golovin <boss@primestr.ru>
2023-12-05 02:09:33 +0500

    Add send_email logic to analyze celery task.

--- backend/api/v1/tasks.py
+++ backend/api/v1/tasks.py
@@ -1,41 +1,78 @@
 import asyncio
 import logging
 
+from django.core.mail import send_mail
 from telegram import Bot
+from telegram.error import BadRequest, InvalidToken, NetworkError, TimedOut
 
 from backend.celery import app
+from backend.settings import DEFAULT_FROM_EMAIL
 from core.environment import BOT_TOKEN
 from products.models import MatchingPredictions
 
+bot = Bot(token=BOT_TOKEN)
 logger = logging.getLogger(__name__)
 
 
 @app.task
-async def send_telegram_message(chat_id, message):
-    bot = Bot(token=BOT_TOKEN)
-    if chat_id is not None:
-        try:
-            await bot.send_message(chat_id=chat_id, text=message)
-            logger.info('Сообщение в Telegram успешно отправлено!')
-        except Exception as err:
-            logger.error(f'Сообщение не отправлено! Текст ошибки: {err}')
+async def send_telegram_message(chat_id, email, message):
 
-    # TODO: Если у пользователя не задан TELEGRAM_ID - отправить письмо
-    #  на почту.
-    logger.warning(f'Сообщение не отправлено! '
-                   f'Не задан Telegram ID пользователя! '
-                   f'Отправляем письмо на почту...')
+    def _telegram_error(error_message, error_detail):
+        return ('Сообщение в Telegram не отправлено! '
+                f'{error_message}!'
+                ' Отправляем письмо на почту...'
+                f' Детали ошибки: {error_detail}')
 
     try:
-        # TODO: Отправить письмо
-        pass
+        async with bot:
+            await bot.send_message(chat_id=chat_id, text=message)
+        logger.info('Сообщение в Telegram успешно отправлено!')
+
+        return
+
+    except BadRequest as err:
+        msg = 'chat_id пользователя не задан! '
+        error = err
+
+    except InvalidToken as err:
+        msg = 'Недействительный токен! '
+        error = err
+
+    except TimedOut as err:
+        msg = 'Истекло время запроса! '
+        error = err
+
+    except NetworkError as err:
+        msg = 'Проверьте возможность подключения к серверам Telegram! '
+        error = err
+
+    except Exception as err:
+        msg = 'Произошла непредвиденная ошибка! '
+        error = err
+
+    # Логируем ошибку.
+    logger.error(_telegram_error(msg, error))
+
+    try:
+        subject = 'Анализ данных Prosept'
+        message = (f'Привет, дорогой друг. Это письмо пришло тебе '
+                   f'потому что ты запустил процесс анализа данных в сервисе '
+                   f'соответствий.\n\nСпешим сообщить тебе, что анализ прошёл '
+                   f'успешно. При следующем запросе тебе будут предоставлены '
+                   f'обновлённые данные.\n\nПриятного тебе трудового дня! '
+                   f'P.S. Не забывай прерываться на чай :)\n\nС любовью, '
+                   f'Команда 4 <3')
+        send_from = DEFAULT_FROM_EMAIL
+        send_to = [email]
+
+        send_mail(subject, message, send_from, send_to, fail_silently=False)
+
     except Exception as error:
-        pass
-        # TODO: Дать в логи ошибку если не отправилось.
+        logger.error(f'Непредвиденная ошибка отправки письма: {error}')
 
 
-@app.task
-def make_predictions(dealer_data, chat_id=None):
+@app.task(rate_limit='1/m')
+def make_predictions(parser, products, matches, email, chat_id=None):
     try:
         # TODO: Сюда ставим вызов ML модели, передаём в неё массив для анализа.
 
@@ -58,4 +95,4 @@ def make_predictions(dealer_data, chat_id=None):
         message = f'Расчёт завершён с ошибкой: {error}'
         logger.error(message)
 
-    asyncio.run(send_telegram_message(chat_id, message))
+    asyncio.run(send_telegram_message(chat_id, email, message))
