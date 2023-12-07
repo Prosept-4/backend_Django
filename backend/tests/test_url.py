e00dfd3f2361f6a57f50faffba0299f76b927b7e
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-12-08 01:49:10 +0300

    add auth test

--- backend/tests/test_url.py
+++ backend/tests/test_url.py
@@ -1,16 +1,12 @@
-from django.test import TestCase
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
-import pytest
-from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from django.test.client import Client
+from rest_framework_simplejwt.tokens import RefreshToken
 
- 
+
 class AuthURLsTestCase(APITestCase):
     def setUp(self):
-        # Создаем пользователя в базе данных
+        """Create test_user"""
         self.user = get_user_model().objects.create_user(
             email='example@name.ru',
             password='example_password',
@@ -18,30 +14,39 @@ class AuthURLsTestCase(APITestCase):
             last_name='example_last_name',
         )
         self.client = APIClient()
-        
+        self.client.force_authenticate(user=None)
+
     def test_login_url(self):
+        """Test user login."""
         response = self.client.post('/api/auth/login/', {
             'email': 'example@name.ru',
             'password': 'example_password'
         }, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Извлекаем токен из ответа и устанавливаем его в HTTP_AUTHORIZATION
         token = response.data.get('access')
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-    
-    def test_logout_url(self):        
+
+    def test_logout_url(self):
+        """Test user logout."""
+        self.client.force_authenticate(user=self.user)
         response = self.client.post('/api/auth/logout/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_token_verify_url(self):
-        response = self.client.post('/api/auth/token/verify/', {'token': 'access_token'}, format='json')
+        """Test user token verify."""
+        refresh = RefreshToken.for_user(self.user)
+        response = self.client.post(
+            '/api/auth/token/verify/',
+            {'token': str(refresh.access_token)}, format='json'
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_token_refresh_url(self):
-        response = self.client.post('/api/auth/token/refresh/', {'refresh': 'refresh_token'}, format='json')
+        """Test user token refresh."""
+        refresh = RefreshToken.for_user(self.user)
+        response = self.client.post(
+            '/api/auth/token/refresh/',
+            {'refresh': str(refresh)}, format='json'
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
-    
-        
